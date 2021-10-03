cecf98e45b95e2838560223f4ad493bc43fb48d1
Christopher-Force <87559500+Christopher-Force@users.noreply.github.com>
2021-10-03 11:48:19 -0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,31 @@
+var button = document.getElementById("enter");
+var input = document.getElementById("userinput");
+var ul = document.querySelector("ul");
+
+function inputLength() {
+	return input.value.length;
+}
+
+function createListElement() {
+	var li = document.createElement("li");
+	li.appendChild(document.createTextNode(input.value));
+	ul.appendChild(li);
+	input.value = "";
+}
+
+function addListAfterClick() {
+	if (inputLength() > 0) {
+		createListElement();
+	}
+}
+
+function addListAfterKeypress(event) {
+	if (inputLength() > 0 && event.keyCode === 13) {
+		console.log("item added was: " + input.value);
+		createListElement();
+	}
+}
+
+button.addEventListener("click", addListAfterClick)
+
+input.addEventListener("keypress", addListAfterKeypress)
